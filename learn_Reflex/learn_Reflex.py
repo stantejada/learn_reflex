c18472bb2298b7cea55041bde3e931f1b3318389
Diego Tejada <stanleytejadaj@gmail.com>
2024-06-05 18:32:53 -0700

    course of reflex by mouredev

---
+++ learn_Reflex/learn_Reflex.py
@@ -0,0 +1,15 @@
+import reflex as rx
+
+
+class State(rx.State):
+    pass
+
+def index() -> rx.Component:
+    return rx.text("Hola reflex!",
+    color="blue")
+
+
+app =rx.App()
+
+app.add_page(index)
+app._compile()
